711ab8e8685c06afd59331bc476a5614887ee3f5
Alex Ward <alxwrd@googlemail.com>
2019-01-15 17:19:38 +0000

    allow fetching stop by stop code

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
 
-const url = "https://cors-anywhere.herokuapp.com/https://www.trentbarton.co.uk/RTILiveTimings.aspx?m=GetRtiFull";
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
+const rootUrl = "https://www.trentbarton.co.uk/RTILiveTimings.aspx";
+
+const getAllStops = "?m=GetStopsForMap&isService=false";
+const getStop = "?m=GetRtiFull&stop=";
 
 
-(function updateDetails() {
-  fetch(url + "&stop=15717",
+function getStoreageItem(name, defaultValue) {
+    var result = JSON.parse(window.localStorage.getItem(name));
+    if (result) {
+        return result
+    } else {
+        return defaultValue;
+    }
+};
+
+
+function setStoreageItem(name, value) {
+    window.localStorage.setItem(name, JSON.stringify(value));
+};
+
+
+(function getStops() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const lastStopData = getStoreageItem("lastStopData", {})
+  var stopCode = (urlParams.get('stop') || "").toLowerCase();
+
+  if (stopCode === lastStopData.C) {
+    updateDetails(lastStopData.Id);
+    return;
+  };
+
+  if (!stopCode && lastStopData.Id) {
+    updateDetails(lastStopData.Id);
+    return;
+  }
+
+  fetch(corsProxy + rootUrl + getAllStops,
+    {
+      method: "GET"
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      data = data.filter(stopData => stopData.C === stopCode);
+
+      if (data.length) {
+        var stopDetails = data[0];
+      } else {
+        var stopDetails = {C: "ntmatjtp", Id: 15717}
+      };
+
+      updateDetails(stopDetails.Id);
+      setStoreageItem("lastStopData", stopDetails);
+    })
+})();
+
+
+function updateDetails(forStopId) {
+  fetch(corsProxy + rootUrl + getStop + forStopId,
     {
       method: "GET"
     })
@@ -25,9 +79,9 @@ const url = "https://cors-anywhere.herokuapp.com/https://www.trentbarton.co.uk/R
           addRowTo("duein", bus.dueIn)
         })
       }
-      setTimeout(updateDetails, 15000)
+      setTimeout(() => {updateDetails(forStopId)}, 15000)
     })
-})();
+};
 
 
 (function updateTime() {
