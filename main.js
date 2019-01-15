0648bde6cfea018cede61de9b9839401f9cd7bfd
Alex Ward <alxwrd@googlemail.com>
2019-01-15 11:41:51 +0000

    use settimeout instead of setinterval
    this allows the function to be self calling and allows for no
    delay fetching the data. because the call is made straight
    away, the time between calls can be increased

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 
-const url = "https://cors-anywhere.herokuapp.com/https://www.trentbarton.co.uk/RTILiveTimings.aspx?m=GetRTI"
+const url = "https://cors-anywhere.herokuapp.com/https://www.trentbarton.co.uk/RTILiveTimings.aspx?m=GetRTI";
 
-setInterval(
-  () => {
-    fetch(url + "&service=65&stop=15717",
-      {
-        method: "GET"
-      })
-      .then((resp) => resp.json())
-      .then((data) => {
-        let rows = document.querySelectorAll(".row")
 
-        if (rows) {
-          rows.forEach((row) => {
-            row.parentNode.removeChild(row);
-          })
-        }
+(function updateDetails() {
+  fetch(url + "&service=65&stop=15717",
+    {
+      method: "GET"
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      let rows = document.querySelectorAll(".row")
 
-        if (data.result) {
-          data.result.forEach((bus) => {
-            addRowTo("service", bus.serviceDisplayName)
-            addRowTo("duein", bus.dueIn)
-          })
-        }
-      })
-  }, 5000);
+      if (rows) {
+        rows.forEach((row) => {
+          row.parentNode.removeChild(row);
+        })
+      }
+
+      if (data.result) {
+        data.result.forEach((bus) => {
+          addRowTo("service", bus.serviceDisplayName)
+          addRowTo("duein", bus.dueIn)
+        })
+      }
+      setTimeout(updateDetails, 15000)
+    })
+})();
 
 
 function addRowTo(elementId, text) {
